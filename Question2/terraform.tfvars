f9c3b9f349a2b07fdee225c34a191a7d39f8071b
Vaibhav Baraskar <83910120+vaibhav77-2000@users.noreply.github.com>
2024-02-12 11:53:32 +0530

    Add files via upload

---
+++ Question2/terraform.tfvars
@@ -0,0 +1,7 @@
+aws_region         = "ap-south-1"
+ssh_key_name       = "demo"
+instance_type      = "t2.micro"
+subnet_id          = "subnet-090553e5e228cd800"
+security_group_id  = "sg-020921694248c1ce4"
+vpc_id             = "vpc-0048122967d683320"
+
---
+++ Question2/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "ssh_key_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "security_group_id" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+
